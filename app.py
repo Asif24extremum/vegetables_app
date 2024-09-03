2f69bb67c23ad82001d4aef46c8bbee34ca5dd89
Asif24extremum <157344114+Asif24extremum@users.noreply.github.com>
2024-09-03 12:38:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -579,7 +579,9 @@ def main(selected_websites, search_terms):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
-    options.add_argument('--window-size=1920,1200')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
     # Use ChromeDriverManager to download and use the correct version of ChromeDriver
