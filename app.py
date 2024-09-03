bb1fe2d019ca6a6cfff35d5b3c585964332fd705
Asif24extremum <157344114+Asif24extremum@users.noreply.github.com>
2024-09-03 16:22:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import (
